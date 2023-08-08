1eeb465c0408ea38e815a7f629e22d037995c9a2
Harshiv Chandra <79366050+chcheetah@users.noreply.github.com>
2023-08-08 22:30:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,66 +1,65 @@
 // create web server
-// create a simple web server
-// var http = require('http');
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.end('Hello World!');
-// }).listen(8080); 
-// Compare this snippet from comments.js:
-// function skillsComments() { 
-//     console.log("I am a member of the comments object");
-// }
-// Compare this snippet from index.js:
-// var skills = require('./skills');
-// skills.skillsMember();
-// skills.skillsComments();
-// var comments = require('./comments');
-// comments.commentsMember();
-// comments.commentsComments();
-// Compare this snippet from skills.js:
-// function skillsMember() { 
-//     console.log("I am a member of the skills object");
-// }
-// function skillsComments() { 
-//     console.log("I am a member of the comments object");
-// }
-// module.exports.skillsMember = skillsMember;
-// module.exports.skillsComments = skillsComments; 
-// Compare this snippet from comments.js:
-// function commentsMember() { 
-//     console.log("I am a member of the comments object");
-// }
-// function commentsComments() { 
-//     console.log("I am a member of the comments object");
-// }
-// module.exports.commentsMember = commentsMember;
-// module.exports.commentsComments = commentsComments; 
-// Compare this snippet from index.js:
-// var skills = require('./skills');
-// skills.skillsMember();
-// skills.skillsComments();
-// var comments = require('./comments');
-// comments.commentsMember();
-// comments.commentsComments();
-// Compare this snippet from skills.js:
-// function skillsMember() { 
-//     console.log("I am a member of the skills object");
-// }
-// function skillsComments() { 
-//     console.log("I am a member of the comments object");
-// }
-// module.exports.skillsMember = skillsMember;
-// module.exports.skillsComments = skillsComments; 
-// Compare this snippet from comments.js:
-// function commentsMember() { 
-//     console.log("I am a member of the comments object");
-// }
-// function commentsComments() { 
-//     console.log("I am a member of the comments object");
-// }
-// module.exports.commentsMember = commentsMember;
-// module.exports.commentsComments = commentsComments; 
-// Compare this snippet from index.js:
-// var skills = require('./skills');
-// skills.skillsMember();
-// skills.skillsComments();
-// var comments = require('./
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
+// //create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     res.end('Hello World\n');
+// });
+// server.listen(4242, () => {
+//     console.log('Server is running...');
+// });
